daa0e93945fdc14fcf996032896ba54e86caf7d0
Patryk <krasowski.p@outlook.com>
2024-06-25 19:48:52 +0200

    handling diffrent states while adding new event

--- src/components/Events/NewEvent.jsx
+++ src/components/Events/NewEvent.jsx
@@ -1,30 +1,43 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom'
 
-import Modal from '../UI/Modal.jsx';
-import EventForm from './EventForm.jsx';
-import { useMutation } from '@tanstack/react-query';
+import Modal from '../UI/Modal.jsx'
+import EventForm from './EventForm.jsx'
+import ErrorBlock from '../UI/ErrorBlock.jsx'
+import { useMutation } from '@tanstack/react-query'
+import { createNewEvent } from '../../util/http.js'
 
 export default function NewEvent() {
-  const navigate = useNavigate();
+	const navigate = useNavigate()
 
-useMutation({
-  mutationFn:
-})
+	const { mutate, isPending, isError, error } = useMutation({
+		mutationFn: createNewEvent,
+	})
 
-  function handleSubmit(formData) {}
+	function handleSubmit(formData) {
+		mutate({ event: formData })
+	}
 
-  return (
-    <Modal onClose={() => navigate('../')}>
-      <EventForm onSubmit={handleSubmit}>
-        <>
-          <Link to="../" className="button-text">
-            Cancel
-          </Link>
-          <button type="submit" className="button">
-            Create
-          </button>
-        </>
-      </EventForm>
-    </Modal>
-  );
+	return (
+		<Modal onClose={() => navigate('../')}>
+			<EventForm onSubmit={handleSubmit}>
+				{isPending && 'Submitting...'}
+				{!isPending && (
+					<>
+						<Link to='../' className='button-text'>
+							Cancel
+						</Link>
+						<button type='submit' className='button'>
+							Create
+						</button>
+					</>
+				)}
+			</EventForm>
+			{isError && (
+				<ErrorBlock
+					title='Failed to create event'
+					message={error.info?.message || 'Failed to create event. Please check your inputs and try again later.'}
+				/>
+			)}
+		</Modal>
+	)
 }
