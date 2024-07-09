cb8a0dddfd58d7222a4f2e15a18727412da08d6e
Patryk <krasowski.p@outlook.com>
2024-07-09 20:32:16 +0200

    use optymistic update

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate, useParams } from 'react-router-dom'
 import Modal from '../UI/Modal.jsx'
 import EventForm from './EventForm.jsx'
 import { useMutation, useQuery } from '@tanstack/react-query'
-import { fetchEvent, updateEvent } from '../../util/http.js'
+import { fetchEvent, queryClient, updateEvent } from '../../util/http.js'
 import ErrorBlock from '../UI/ErrorBlock.jsx'
 import LoadingIndicator from '../UI/LoadingIndicator.jsx'
 
@@ -11,7 +11,26 @@ export default function EditEvent() {
 	const navigate = useNavigate()
 	const { id } = useParams()
 
-	const { mutate } = useMutation({ mutationFn: updateEvent })
+	const { data, isPending, isError, error } = useQuery({
+		queryKey: ['events', id],
+		queryFn: ({ signal }) => fetchEvent({ signal, id }),
+	})
+	const { mutate } = useMutation({
+		mutationFn: updateEvent,
+		onMutate: async data => {
+			const newEvent = data.event
+			await queryClient.cancelQueries({ queryKey: ['events'], id })
+			const previousEvent = queryClient.getQueryData(['events', id])
+			queryClient.setQueryData(['events', id], newEvent)
+			return { previousEvent }
+		},
+		onError: (error, data, context) => {
+			queryClient.setQueryData(['events', id], context.previousEvent)
+		},
+		onSettled: () => {
+			queryClient.invalidateQueries(['events', id])
+		},
+	})
 
 	function handleSubmit(formData) {
 		mutate({ id, event: formData })
@@ -21,11 +40,6 @@ export default function EditEvent() {
 	function handleClose() {
 		navigate('../')
 	}
-
-	const { data, isPending, isError, error } = useQuery({
-		queryKey: ['events', id],
-		queryFn: ({ signal }) => fetchEvent({ signal, id }),
-	})
 
 	return (
 		<Modal onClose={handleClose}>
